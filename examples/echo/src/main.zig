9850c725579040656051951f96cc5602538831ff
Kah Wai <kwlum80@gmail.com>
2023-02-23 09:26:22 +0800

    update todos for latest changes in kit

--- examples/echo/src/main.zig
+++ examples/echo/src/main.zig
@@ -4,9 +4,9 @@ const http = std.http;
 const kit = @import("wws-zig-kit");
 
 fn handle(_: void, request: kit.Request, response: *kit.Response, _: *kit.Cache) !void {
-    response.status = http.Status.ok;
     try response.headers.put("content-type", "text/plain");
     try response.body.writeAll(request.body);
+    response.status = http.Status.ok;
 }
 
 pub fn main() !void {
